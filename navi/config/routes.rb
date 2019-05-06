782c5279ed2583612312fd09c3f9c1d40d68d06e
鮎田翔平 <shoheiayuda@AyushonoMacBook-Pro.local>
2019-05-06 10:47:53 +0900

    progate模写

--- navi/config/routes.rb
+++ navi/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index' => "posts#index"
+
+  get "top" => "home#top"
+  get "/" => "home#about"
+  get "about" => "home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
